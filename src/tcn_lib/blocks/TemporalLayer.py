8b029b243b41855facd614efb9975753fd39b2b0
Douwe den Blanken <douwedb@gmail.com>
2023-10-20 10:53:04 +0200

    Add temporal layer code (which I forgot to include for last commit)

---
+++ src/tcn_lib/blocks/TemporalLayer.py
@@ -0,0 +1,62 @@
+"""File contents taken from:
+
+https://github.com/locuslab/TCN/blob/2f8c2b817050206397458d
+fd1f5a25ce8a32fe65/TCN/tcn.py.
+"""
+
+import torch
+import torch.nn as nn
+from torch.nn.utils import weight_norm
+
+from metalarena.models.blocks.Chomp1d import Chomp1d
+from metalarena.models.utils import conditional_apply, init_tcn_conv_weight
+
+
+class TemporalLayer(nn.Sequential):
+
+    def __init__(self,
+                 n_inputs: int,
+                 n_outputs: int,
+                 kernel_size: int,
+                 stride: int,
+                 dilation: int,
+                 padding: int,
+                 dropout=0.2,
+                 batch_norm=False,
+                 use_weight_norm=False,
+                 with_activation=True,
+                 groups=1):
+        # We apply padding on both sides and remove the extra values on the right side
+        # We could have also only applied padding on the left side like here:
+        # https://github.com/locuslab/TCN/issues/8#issuecomment-384345206
+        conv = conditional_apply(weight_norm, use_weight_norm)(
+            nn.Conv1d(
+                n_inputs,
+                n_outputs,
+                kernel_size,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+                groups=groups,
+                # Following:
+                # https://pytorch.org/tutorials/recipes/recipes/tuning_guide.html#disable-bias-for-convolutions-directly-followed-by-a-batch-norm
+                bias=not batch_norm))
+        chomp = Chomp1d(padding)
+        normalize = nn.BatchNorm1d(n_outputs) if batch_norm else nn.Identity()
+        relu = nn.ReLU(inplace=True) if with_activation else nn.Identity()
+        dropout = nn.Dropout(dropout)
+
+        init_tcn_conv_weight(conv)
+
+        super(TemporalLayer, self).__init__(conv, normalize, chomp, relu,
+                                            dropout)
+
+    def fuse_model(self):
+        modules_to_fuse = ['0']
+
+        if isinstance(self[1], nn.BatchNorm1d):
+            modules_to_fuse.append('1')
+        if isinstance(self[3], nn.ReLU):
+            modules_to_fuse.append('3')
+
+        torch.quantization.fuse_modules(self, [modules_to_fuse], inplace=True)
